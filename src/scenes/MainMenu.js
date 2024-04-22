60e7498af2867c627d9630880af449ce7e6f30ad
TorresjDev <torres.jesus2143@gmail.com>
2024-04-22 02:30:42 -0500

    first commit: phaser game

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,28 +1,28 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class MainMenu extends Scene
-{
-    constructor ()
-    {
-        super('MainMenu');
-    }
+export class MainMenu extends Scene {
+  constructor() {
+    super("MainMenu");
+  }
 
-    create ()
-    {
-        this.add.image(512, 384, 'background');
+  create() {
+    this.add.image(512, 384, "background");
 
-        this.add.image(512, 300, 'logo');
+    this.add.image(512, 300, "logo");
 
-        this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+    this.add
+      .text(512, 460, "CLICK TO START", {
+        fontFamily: "Arial Black",
+        fontSize: 38,
+        color: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: 8,
+        align: "center",
+      })
+      .setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('Game');
-
-        });
-    }
+    this.input.once("pointerdown", () => {
+      this.scene.start("Game");
+    });
+  }
 }
